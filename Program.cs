a07f23415f6dc49f483a1129604cf2c83f5abca2
Rafael Tongson <rtongson@peppymiint.local>
2019-10-14 14:19:36 -0700

    unbox and add to sum

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BoxingUnboxing
 {
@@ -6,7 +7,23 @@ namespace BoxingUnboxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<object> list = new List<object>();
+            list.Add(7);
+            list.Add(28);
+            list.Add(-1);
+            list.Add(true);
+            list.Add("chair");
+
+            int sum = 0;
+            for(int idx = 0; idx < list.Count; idx++)
+            {
+                if(list[idx] is int)
+                {
+                    sum+=(int)list[idx];
+                }
+            }
+
+            Console.WriteLine(sum);    
         }
     }
 }
